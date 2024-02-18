d44ff4a25af2fe54160f89df50b95f90b45bd55c
Tanya <pazhun2007@gmail.com>
2024-02-18 16:46:15 +0200

    finish

--- main.py
+++ main.py
@@ -1,16 +1,107 @@
-# This is a sample Python script.
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+menu = {
+    "Еспресо": 20,
+    "Рістрето": 23,
+    "Лате": 30,
+    "Капучіно": 30,
+    "Американо": 25,
+    "Допіо": 35,
+}
+
+def show_menu():
+    for item, price in menu.items():
+        print(item, '-', price)
+
+def add_to_menu(name, price):
+    menu[name] = price
+    show_menu()
+
+def calculate_order(order, menu):
+    total_cost = 0
+    print("Your order:")
+    for item, quantity in order.items():
+        item_price = menu[item]
+        item_cost = item_price * quantity
+        total_cost += item_cost
+        print(f"{item}: {quantity} x {item_price} = {item_cost}")
+
+    print(f"Total amount: {total_cost}")
+
+def checkOrder(orderName):
+    return isinstance(orderName, str) and orderName in menu
+
+def is_number(s):
+    try:
+        int(s)
+        return True
+    except ValueError:
+        try:
+            float(s)
+            return True
+        except ValueError:
+            try:
+                complex(s)
+                return True
+            except ValueError:
+                return False
+
+def to_number(s):
+    try:
+        return int(s)
+    except ValueError:
+        try:
+            return float(s)
+        except ValueError:
+            try:
+                return complex(s)
+            except ValueError:
+                return False
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+def main():
+    choice = ""
+    order = {}
+    show_menu()
+
+    while True:
+
+        if len(choice) == 0:
+            choice = input("To choose your coffee please type the name:")
+            if not checkOrder(choice):
+                print("Only string are allowed or there is no such item, please try again")
+                choice = input("If you wish to add something new to menu type 'new' or press any bottom to skip")
+                if choice == "new":
+                    name = input("Enter name")
+                    price = int((input("Enter price")))
+                    add_to_menu(name, price)
+
+                choice = ""
+                continue
+
+        if choice == "end":
+            break
+        elif choice == "new":
+            print("new")
+
+        else:
+            amount = input("Enter amount: ")
+            if is_number(amount):
+                order[choice] = order.get(choice, 0) + to_number(amount)
+                choice = input("If you wish to continue type 'Y' if yon want to finish press any bottom")
+                if choice == "Y":
+                    choice = ""
+                    continue
+                else:
+                    break
+
+
+            else:
+                print("Only numbers are allowed, please try again")
+                continue
+
+    calculate_order(order, menu)
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    main()
